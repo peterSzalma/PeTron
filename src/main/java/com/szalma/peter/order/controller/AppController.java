db675cc1c3e36bcb9a1a4da0082bfc05922ed6b2
peterSzalma <petiszalma@gmail.com>
2019-06-05 20:43:18 +0200

    add login and registration pages

--- src/main/java/com/szalma/peter/order/controller/AppController.java
+++ src/main/java/com/szalma/peter/order/controller/AppController.java
@@ -2,6 +2,7 @@ package com.szalma.peter.order.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -9,5 +10,15 @@ public class AppController {
     @GetMapping("/")
     public String landingPage() {
         return "landing_page";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/registration")
+    public String registrationPage() {
+        return "registration";
     }
 }
