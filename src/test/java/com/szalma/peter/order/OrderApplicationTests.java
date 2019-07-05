d2c892607af37b64af612e3ff0cf4a5c1e9ee37d
peterSzalma <petiszalma@gmail.com>
2019-07-05 10:40:02 +0200

    make a test which is tested that a customer saved into the database

--- src/test/java/com/szalma/peter/order/OrderApplicationTests.java
+++ src/test/java/com/szalma/peter/order/OrderApplicationTests.java
@@ -1,16 +1,40 @@
 package com.szalma.peter.order;
 
+import com.szalma.peter.order.entity.Customer;
+import com.szalma.peter.order.repository.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class OrderApplicationTests {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @Test
-    public void contextLoads() {
+    public void saveOneCustomer() {
+        Customer customer = Customer.builder()
+                .companyName("Test Kft.")
+                .email("test@gmail.com")
+                .phoneNumber("2000000")
+                .password("sdflkgjfd")
+                .build();
+
+        customerRepository.save(customer);
+
+        List<Customer> customerList = customerRepository.findAll();
+        assertThat(customerList).hasSize(1);
     }
 
 }
